a74d8504eb997782ba4965f63623edf941f56886
Vinni Kapoor <vinni3871@gmail.com>
2025-01-08 14:10:10 +0000

    [LeetCode Sync] Runtime - 2 ms (96.50%), Memory - 42.8 MB (100.00%)

---
+++ solutions/1468-check-if-n-and-its-double-exist/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        int size=arr.length;
+        for(int i=0;i<size;i++) {
+            for(int j=0;j<size;j++) {
+                if(i!=j && arr[i]==(2*arr[j])) return true;
+            }
+        }
+        return false;
+    }
+}
