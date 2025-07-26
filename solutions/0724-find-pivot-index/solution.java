a6b1245881b8d3a89fae86ee253dd0afa4fdffe4
Vinni Kapoor <vinni3871@gmail.com>
2025-07-26 18:49:21 +0000

    [LeetCode Sync] Runtime - 1 ms (96.23%), Memory - 45.7 MB (99.94%)

--- solutions/0724-find-pivot-index/solution.java
+++ solutions/0724-find-pivot-index/solution.java
@@ -1,35 +1,20 @@
 class Solution {
     public int pivotIndex(int[] nums) {
 
-        int res = -1;
+        int totalSum = 0;
+
         for(int i = 0; i < nums.length; i++) {
-            if(calculateSum(i, nums)) {
-                res = i;
-                return res;
-            }
+            totalSum += nums[i];
         }
-
-        return res;
-        
-    }
-
-    public boolean calculateSum(int index, int[] nums) {
 
         int sumL = 0;
-        int sumR = 0;
 
         for(int i = 0; i < nums.length; i++) {
-            if(i < index) {
-                sumL += nums[i];
-            }
-
-            if(i > index) {
-                sumR += nums[i];
-            }
+            int sumR = totalSum - sumL - nums[i];
+            if(sumL == sumR) return i;
+            sumL += nums[i];
         }
 
-        if(sumL == sumR) return true;
-
-        return false;
+        return -1;
     }
 }
