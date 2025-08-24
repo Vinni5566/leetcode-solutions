63dfe2c4e076cf37649890824cc1a6dd9e1c63ee
Vinni Kapoor <vinni3871@gmail.com>
2025-08-24 08:51:13 +0000

    [LeetCode Sync] Runtime - 4 ms (100.00%), Memory - 43.9 MB (99.98%)

--- solutions/0009-palindrome-number/solution.java
+++ solutions/0009-palindrome-number/solution.java
@@ -1,12 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0) return false;
+        if(x < 0) return false;
+
         int original=x, rev=0;
-        while(x>0) {
-            int n1=x%10;
-            rev=(rev*10)+n1;
+
+        while(x > 0) {
+            int rem = x % 10;
+            rev = rev*10 + rem;
             x /= 10;
         }
-        return original==rev;
+
+        return original == rev;
     }
 }
