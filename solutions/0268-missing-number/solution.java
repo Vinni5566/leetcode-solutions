c75d351022193616fac99525aafd718c47957e7b
Vinni Kapoor <vinni3871@gmail.com>
2025-08-25 18:17:15 +0000

    [LeetCode Sync] Runtime - 7 ms (16.79%), Memory - 45.9 MB (99.91%)

--- solutions/0268-missing-number/solution.java
+++ solutions/0268-missing-number/solution.java
@@ -1,14 +1,16 @@
 class Solution {
     public int missingNumber(int[] nums) {
 
-        Arrays.sort(nums);
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int num : nums) {
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
 
         int res = 0;
 
-        for(int i =0; i < nums.length; i++) {
-            if(nums[i] != res ) return res;
-
-            res++;
+        for(int i = 0; i <= nums.length; i++) {
+            if(map.getOrDefault(i, 0) == 0) res = i;
         }
 
         return res;
