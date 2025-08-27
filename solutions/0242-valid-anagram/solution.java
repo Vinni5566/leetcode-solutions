227afcff2e7507d845291f1d94f98f815468627a
Vinni Kapoor <vinni3871@gmail.com>
2025-08-27 18:20:51 +0000

    [LeetCode Sync] Runtime - 16 ms (20.75%), Memory - 45 MB (50.30%)

--- solutions/0242-valid-anagram/solution.java
+++ solutions/0242-valid-anagram/solution.java
@@ -1,11 +1,26 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] arr1 = s.toCharArray();
-        char[] arr2 = t.toCharArray();
+        int n = s.length();
+        int m = t.length();
 
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
+        if(n != m) return false;
 
-        return Arrays.equals(arr1, arr2);
+        HashMap<Character, Integer> s1 = new HashMap<>();
+        HashMap<Character, Integer> s2 = new HashMap<>();
+
+        for(int i = 0; i < n; i++) {
+            s1.put(s.charAt(i), s1.getOrDefault(s.charAt(i), 0)+1);
+        }
+
+        for(int i = 0; i < m; i++) {
+            s2.put(t.charAt(i), s2.getOrDefault(t.charAt(i), 0)+1);
+        }
+
+        for(char c : s1.keySet()) {
+            int freq = s2.getOrDefault(c, 0);
+            if(freq != s1.get(c)) return false;
+        }
+
+        return true;
     }
 }
