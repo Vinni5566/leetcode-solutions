21b3c48ca27908c8c3a57aaef6269e139a58a591
Vinni Kapoor <vinni3871@gmail.com>
2025-11-30 11:33:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.4 MB (91.32%)

---
+++ solutions/2032-largest-odd-number-in-string/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        
+        if((num[num.length()-1])%2 != 0) return num;
+
+        for(int i = num.length()-1; i >= 0; i--) {
+            int digit = num[i] - '0';
+            if(digit % 2 != 0) {
+                return num.substr(0, i+1);
+            }
+        }
+
+        return "";
+    }
+};
