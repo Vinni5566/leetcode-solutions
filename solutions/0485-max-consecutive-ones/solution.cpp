90e21d295bd0eee9cb5b79db9780dfcff67d01ae
Vinni Kapoor <vinni3871@gmail.com>
2025-12-31 15:05:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 50.3 MB (33.19%)

---
+++ solutions/0485-max-consecutive-ones/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        
+        int maxOnes = 0;
+        int currOnes = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            
+            if(nums[i] == 0) {
+                maxOnes = max(maxOnes, currOnes);
+                currOnes = 0;
+                continue;
+            }
+
+            currOnes++;
+        }
+
+        maxOnes = max(maxOnes, currOnes);
+
+        return maxOnes;
+    }
+};
