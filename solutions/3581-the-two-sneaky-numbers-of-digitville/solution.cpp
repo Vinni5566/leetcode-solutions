38be51ef31b2a298dcecc40346b3ebfcf54e4856
Vinni Kapoor <vinni3871@gmail.com>
2026-02-06 07:59:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 25.5 MB (70.99%)

---
+++ solutions/3581-the-two-sneaky-numbers-of-digitville/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+
+        int n = nums.size();
+        
+        vector<int> res;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < n-1; i++) {
+            if(nums[i] == nums[i+1]) {
+                res.push_back(nums[i]);
+            }
+        }
+
+        return res;
+    }
+};
