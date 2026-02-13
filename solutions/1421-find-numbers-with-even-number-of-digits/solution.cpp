3cfe067971f61923806975cb49aecc8321e41068
Vinni Kapoor <vinni3871@gmail.com>
2026-02-13 18:53:28 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.5 MB (34.32%)

---
+++ solutions/1421-find-numbers-with-even-number-of-digits/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+
+        int count = 0;
+
+        for(int num : nums) {
+            if(num >= 10 && num <= 99) count++;
+            else if(num >= 1000 && num <= 9999) count++;
+            else if(num == 100000) count++;
+        }
+
+        return count;
+        
+    }
+};
