e999c2e8ba6b9ee2aec3aad4682d803d76561ff8
Vinni Kapoor <vinni3871@gmail.com>
2026-02-26 20:32:39 +0000

    [LeetCode Sync] Runtime - 27 ms (59.19%), Memory - 27.6 MB (19.40%)

--- solutions/1250-longest-common-subsequence/solution.cpp
+++ solutions/1250-longest-common-subsequence/solution.cpp
@@ -5,22 +5,18 @@ public:
         int l1 = text1.length();
         int l2 = text2.length();
 
-        if(text1 == text2) return l1;
-
         vector<vector<int>> dp(l1+1, vector<int>(l2+1, 0));
 
-        for(int i = 1; i <= l1; i++) {
-            for(int j = 1; j <= l2; j++) {
-
-                //both are equal
+        for(int i = 1; i < l1+1; i++) {
+            for(int j = 1; j < l2+1; j++) {
                 if(text1[i-1] == text2[j-1]) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    dp[i][j] = dp[i-1][j-1] + 1;
                 } else {
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);  //skip i or skip j
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
 
-        return dp[l1][l2];  //length of LCS   
+        return dp[l1][l2];
     }
 };
