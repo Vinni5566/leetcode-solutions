4ad935f9f8b34a6a1254ffc9445845daeeb1c565
Vinni Kapoor <vinni3871@gmail.com>
2026-03-19 18:52:38 +0000

    [LeetCode Sync] Runtime - 15 ms (49.27%), Memory - 100.8 MB (47.27%)

--- solutions/3338-count-submatrices-with-top-left-element-and-sum-less-than-k/solution.cpp
+++ solutions/3338-count-submatrices-with-top-left-element-and-sum-less-than-k/solution.cpp
@@ -2,19 +2,29 @@ class Solution {
 public:
     int countSubmatrices(vector<vector<int>>& grid, int k) {
 
-        int n = grid.size(), m = grid[0].size();
-        vector<int> cols(m);
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> submatrixSum(n, vector<int>(m));
+        submatrixSum[0][0] = grid[0][0];
+
         int res = 0;
-        for (int i = 0; i < n; i++) {
-            int rows = 0;
-            for (int j = 0; j < m; j++) {
-                cols[j] += grid[i][j];
-                rows += cols[j];
-                if (rows <= k) {
-                    res++;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(i == 0 && j > 0) {
+                    submatrixSum[i][j] = submatrixSum[i][j-1] + grid[i][j];
+                } else if(i > 0 && j == 0) {
+                    submatrixSum[i][j] = submatrixSum[i-1][j]+grid[i][j];
+                } else if(i > 0 && j > 0) {
+                    submatrixSum[i][j] = submatrixSum[i-1][j] + submatrixSum[i][j-1] - submatrixSum[i-1][j-1] + grid[i][j];
                 }
+
+                if(submatrixSum[i][j] <= k) res++;
+                else break;
             }
         }
+
         return res;
         
     }
