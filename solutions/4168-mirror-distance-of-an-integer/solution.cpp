70a8a713aea3a8d06f9d3fa97d0166c182823cf5
Vinni Kapoor <vinni3871@gmail.com>
2026-04-18 14:16:41 +0000

    [LeetCode Sync] Runtime - 1 ms (8.39%), Memory - 8.6 MB (22.62%)

--- solutions/4168-mirror-distance-of-an-integer/solution.cpp
+++ solutions/4168-mirror-distance-of-an-integer/solution.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     int mirrorDistance(int n) {
         
-        int original = n;  //original number
- 
-        int reversed = 0;  //reversed number
+        if(1 <= n && n <= 9) return 0;
+
+        int original = n;
+        int reverse = 0;
 
         while(n > 0) {
             int r = n%10;
-            reversed = (reversed*10) + r;
             n /= 10;
+            reverse = (reverse*10) + r;
         }
 
-        return abs(original - reversed);  //mirror distance
+        return abs(original - reverse);
     }
 };
