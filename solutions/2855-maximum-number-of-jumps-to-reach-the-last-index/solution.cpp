9b3d186fc95463841b0a193e2be6e15619411dac
Vinni Kapoor <vinni3871@gmail.com>
2026-05-10 19:00:46 +0000

    [LeetCode Sync] Runtime - 27 ms (25.15%), Memory - 68.6 MB (41.84%)

---
+++ solutions/2855-maximum-number-of-jumps-to-reach-the-last-index/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maximumJumps(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+
+        vector<int> dp(n, -1);
+        dp[0] = 0;
+
+        for(int i = 1; i < n; i++) {
+            
+            for(int j = i-1; j >= 0; j--) {
+                if(dp[j] != -1 && -1*target <= nums[j]-nums[i] && nums[j]-nums[i] <= target) {
+                    dp[i] = max(dp[i], dp[j]+1);
+                }
+            }
+        }
+
+        return dp[n-1];
+
+
+    }
+};
