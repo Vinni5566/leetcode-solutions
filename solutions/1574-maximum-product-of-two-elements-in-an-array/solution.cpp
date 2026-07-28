8f8a77da7a35f58fbffa2b7c3dd5ca158c91f05d
Vinni Kapoor <vinni3871@gmail.com>
2026-07-28 08:28:41 +0000

    [LeetCode Sync] Runtime - 6 ms (5.61%), Memory - 13.5 MB (20.61%)

---
+++ solutions/1574-maximum-product-of-two-elements-in-an-array/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        int maxProd = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i+1; j < n; j++) {
+                maxProd = max(maxProd, (nums[i]-1)*(nums[j]-1));
+            }
+        }
+
+        return maxProd;
+    }
+};
