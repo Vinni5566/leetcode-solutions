f420f2331d781cf81cc81dad9795fb6997d41247
Vinni Kapoor <vinni3871@gmail.com>
2026-08-22 08:16:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.7 MB (94.63%)

---
+++ solutions/3918-check-divisibility-by-digit-sum-and-product/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        
+        int num = n;
+
+        int sum = 0;
+        int prod = 1;
+
+        while(num > 0) {
+            int r = num%10;
+            sum += r;
+            prod *= r;
+
+            num/=10;
+        }
+
+        if(n % (sum+prod) == 0) return true;
+
+        return false;
+    }
+};
